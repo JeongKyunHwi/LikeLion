dab961fc648c2d22cbed7d8d40a2e9ffc2a383bf
JeongKyunHwi <rbsgnl123@gmail.com>
2025-02-21 14:34:54 +0900

    study5

--- LikeLion1/LikeLion1/Program.cs
+++ LikeLion1/LikeLion1/Program.cs
@@ -10,82 +10,167 @@ namespace LikeLion1
     {
         static void Main(string[] args)
         {
-            //출력
-            Console.WriteLine("Github 연결 하기");
+            ////출력
+            //Console.WriteLine("Github 연결 하기");
 
-            //변수 선언
-            int age; //정수형 변수
-            age = 25;
+            ////변수 선언
+            //int age; //정수형 변수
+            //age = 25;
 
-            Console.WriteLine(age);
+            //Console.WriteLine(age);
 
-            //리터럴 사용하기
-            int number = 10;
-            double pi = 3.14;
-            char letter = 'A';
-            string name = "Alice";
+            ////리터럴 사용하기
+            //int number = 10;
+            //double pi = 3.14;
+            //char letter = 'A';
+            //string name = "Alice";
 
-            Console.WriteLine(number);
-            Console.WriteLine(pi);
-            Console.WriteLine(letter);
-            Console.WriteLine(name);
+            //Console.WriteLine(number);
+            //Console.WriteLine(pi);
+            //Console.WriteLine(letter);
+            //Console.WriteLine(name);
 
-            //캐릭터 hp 100 , att 56.7 , 이름, 등급 S
+            ////캐릭터 hp 100 , att 56.7 , 이름, 등급 S
 
-            int hp = 100;
-            double att = 56.7;
-            name = "Jinwoo";
-            char grade = 'S';
-            Console.WriteLine();
-            Console.WriteLine("캐릭터");
-            Console.WriteLine("hp : " + hp);
-            Console.WriteLine("공격력 : " + att);
-            Console.WriteLine("이름 : " + name);
-            Console.WriteLine("등급 : " + grade);
+            //int hp = 100;
+            //double att = 56.7;
+            //name = "Jinwoo";
+            //char grade = 'S';
+            //Console.WriteLine();
+            //Console.WriteLine("캐릭터");
+            //Console.WriteLine("hp : " + hp);
+            //Console.WriteLine("공격력 : " + att);
+            //Console.WriteLine("이름 : " + name);
+            //Console.WriteLine("등급 : " + grade);
 
-            Console.WriteLine();
-            string greeting;
-            greeting = "Hello, World!";
-            Console.WriteLine(greeting);
+            //Console.WriteLine();
+            //string greeting;
+            //greeting = "Hello, World!";
+            //Console.WriteLine(greeting);
 
-            Console.WriteLine();
-            int score = 100;
-            double temperature = 36.5;
-            string city = "Seoul";
+            //Console.WriteLine();
+            //int score = 100;
+            //double temperature = 36.5;
+            //string city = "Seoul";
 
-            Console.WriteLine(score);
-            Console.WriteLine(temperature);
-            Console.WriteLine(city);
+            //Console.WriteLine(score);
+            //Console.WriteLine(temperature);
+            //Console.WriteLine(city);
 
-            Console.WriteLine();
-            int x = 10, y = 20, z = 30;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            //Console.WriteLine();
+            //int x = 10, y = 20, z = 30;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
 
-            Console.WriteLine();
-            const double Pi = 3.14159;
-            const int MaxScore = 100;
-            Console.WriteLine("Pi : " + Pi);
-            Console.WriteLine("Max Score : " + MaxScore);
+            //Console.WriteLine();
+            //const double Pi = 3.14159;
+            //const int MaxScore = 100;
+            //Console.WriteLine("Pi : " + Pi);
+            //Console.WriteLine("Max Score : " + MaxScore);
 
-            Console.WriteLine();
-            //로스트아크 캐릭터 스탯
-            double level = 1391.66;
-            int attack = 16755, maxHp = 78103, Cri = 36, a=1017, b=41, c=611, d=22, e=39;
+            //Console.WriteLine();
+            ////로스트아크 캐릭터 스탯
+            //double level = 1391.66;
+            //int attack = 16755, maxHp = 78103, Cri = 36, a = 1017, b = 41, c = 611, d = 22, e = 39;
 
-            Console.WriteLine("로스트아크 캐릭터 스텟");
-            Console.WriteLine("아이템 레벨 : " + level);
-            Console.WriteLine("공격력 : " + attack);
-            Console.WriteLine("최대 생명력 : " + maxHp);
-            Console.WriteLine("치명 : " + Cri);
-            Console.WriteLine("특화 : " + a);
-            Console.WriteLine("제압 : " + b);
-            Console.WriteLine("신속 : " + c);
-            Console.WriteLine("인내 : " + d);
-            Console.WriteLine("숙련 : " + e);
+            //Console.WriteLine("로스트아크 캐릭터 스텟");
+            //Console.WriteLine("아이템 레벨 : " + level);
+            //Console.WriteLine("공격력 : " + attack);
+            //Console.WriteLine("최대 생명력 : " + maxHp);
+            //Console.WriteLine("치명 : " + Cri);
+            //Console.WriteLine("특화 : " + a);
+            //Console.WriteLine("제압 : " + b);
+            //Console.WriteLine("신속 : " + c);
+            //Console.WriteLine("인내 : " + d);
+            //Console.WriteLine("숙련 : " + e);
 
+            //Console.WriteLine();
+            //int integerNum = 10;
+            //float floatNum = 3.14f;
+            //double doubleNum = 3.14159;
 
+            //Console.WriteLine(integerNum);
+            //Console.WriteLine(floatNum);
+            //Console.WriteLine(doubleNum);
+
+            //int intValue = -100;
+            //long longV = 1234567890L;
+            //Console.WriteLine(intValue);
+            //Console.WriteLine(longV);
+
+            //sbyte signedByte = -50;
+            //short signedShort = -32000;
+            //int signedInt = -20000000;
+
+            //Console.WriteLine(signedByte);
+            //Console.WriteLine(signedShort);
+            //Console.WriteLine(signedInt);
+
+            //byte unsignedByte = 255;
+            //ushort unsignedShort = 65000;
+            //uint unsignedInt = 4000000000;
+
+            //Console.WriteLine(unsignedByte);
+            //Console.WriteLine(unsignedShort);
+            //Console.WriteLine(unsignedInt);
+
+            //float singlePrecision = 3.14f;
+            //double doublePrecision = 3.1415926535;
+            //decimal highPrecision = 3.145234234324235235333123113333m;
+
+            //Console.WriteLine(singlePrecision);
+            //Console.WriteLine(doublePrecision);
+            //Console.WriteLine(highPrecision);
+
+            //int integerValue = 100;
+            //long longValue = 100L;
+            //float floatValue = 3.14f;
+            //double doubleValue = 3.14;
+            //decimal decimalValue = 3.14m;
+
+            //Console.WriteLine(integerValue);
+            //Console.WriteLine(longValue);
+            //Console.WriteLine(floatValue);
+            //Console.WriteLine(doubleValue);
+            //Console.WriteLine(decimalValue);
+
+            //char symbol = '#';
+            //char Cnumber = '9';
+
+            //Console.WriteLine(symbol);
+            //Console.WriteLine(Cnumber);
+
+            //Console.WriteLine(greeting + " " + name);
+
+            //bool isRunning = true;
+            //bool isFinished = false;
+
+            //Console.WriteLine(isRunning);
+            //Console.WriteLine(isFinished);
+
+            //const double pi = 3.14;
+            //const int MaxScore = 100;
+
+            //Console.WriteLine(pi);
+            //Console.WriteLine(MaxScore);
+
+            //System.Int32 number = 123;
+            //System.String text = "Hello";
+            //System.Boolean flag = true;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(text);
+            //Console.WriteLine(flag);
+
+            //int number = 123;
+            //string NumAsString = number.ToString();
+
+            //bool flag = true;
+            //string FlagAsString = flag.ToString();
+
+            //Console.WriteLine(NumAsString);
+            //Console.WriteLine(FlagAsString);
 
 
 
