746d9f0e8a3fbd54a85888baec1f3f370d483383
JeongKyunHwi <rbsgnl123@gmail.com>
2025-02-27 17:00:13 +0900

    Task4-1

---
+++ ShootingGame1/ShootingGame1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShootingGame1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+
+            int x = 10, y = 10;
+            Console.CursorVisible = false;
+            ConsoleKeyInfo keyInfo;
+
+            string[] player = new string[]
+            {
+                "->",
+                ">>>",
+                "->",
+            };
+
+            //시간 1초 루프
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            long prev_second = stopwatch.ElapsedMilliseconds; // 1/1000 -> 1000일때 1초
+
+
+            int playerX = 0;
+            int playerY = 12;
+            while (true)
+            {
+                
+                long current_second = stopwatch.ElapsedMilliseconds; // 현재 시간 가져오기
+                if(current_second - prev_second >= 300)
+                {
+                    Console.Clear();
+                    keyInfo = Console.ReadKey(true);
+                    //Console.WriteLine("1초루프");
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.UpArrow: if (playerY > 0) playerY--; break;
+                        case ConsoleKey.DownArrow: if (playerY < Console.WindowHeight - 3) playerY++; break;
+                        case ConsoleKey.LeftArrow: if (playerX > 0) playerX--; break;
+                        case ConsoleKey.RightArrow: if (playerX < Console.WindowWidth - 1) playerX++; break;
+                        case ConsoleKey.Spacebar: Console.Write("미사일키"); break;
+                        case ConsoleKey.Escape: return;
+
+                    }
+                    for (int i = 0; i < player.Length; i++)
+                    {
+                        //콘솔 좌표 설정 플레이어X 플레이어Y
+                        Console.SetCursorPosition(playerX, playerY + i);
+                        //콘솔 문자열 출력
+                        Console.WriteLine(player[i]);
+
+
+                    }
+                    prev_second = current_second; // 이전 시간 업데이트
+                }
+                
+
+                
+                
+
+            }
+            
+
+            //while (true)
+            //{
+            //    Console.Clear();
+
+            //    Console.SetCursorPosition(x, y);
+            //    Console.Write("○"); // 현재 위치 출력
+            //    keyInfo = Console.ReadKey(true);
+
+            //    switch (keyInfo.Key)
+            //    {
+            //        case ConsoleKey.UpArrow: if (y > 0) y--; break;
+            //        case ConsoleKey.DownArrow: if (y < Console.WindowHeight - 1) y++; break;
+            //        case ConsoleKey.LeftArrow: if (x > 0) x--; break;
+            //        case ConsoleKey.RightArrow: if (x < Console.WindowWidth - 1) x++; break;
+            //        case ConsoleKey.Spacebar: Console.Write("미사일키"); break;
+            //        case ConsoleKey.Escape: return;
+
+            //    }
+
+            //}
+
+
+        }
+    }
+}
---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        const int MAX_ITEMS = 10;
+
+        struct Inventory
+        {
+            public string[] Item_Name;
+            public int[] Item_Count;
+
+            public Inventory(int a)
+            {
+                Item_Name = new string[a];
+                Item_Count = new int[a];
+            }
+            public void Add_Item(string s, int c)
+            {
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (Item_Name[i] == s)
+                    {
+                        Item_Count[i] += c;
+                        return;
+                    }
+
+                }
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (Item_Name[i] == null)
+                    {
+                        Item_Name[i] = s;
+                        Item_Count[i] = c;
+                        return;
+                    }
+                }
+                Console.WriteLine("인벤토리 가득 참");
+            }
+            public void Delete_Item(string s, int c)
+            {
+                Console.WriteLine($"{s} {c}개 삭제 시도");
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (Item_Name[i] == s)
+                    {
+                        if (Item_Count[i] >= c)
+                        {
+                            Item_Count[i] -= c;
+                            if (Item_Count[i] == 0)
+                            {
+                                Item_Name[i] = null;
+                            }
+                            return;
+                        }
+                        else if (Item_Count[i] - c < 0)
+                        {
+                            Console.WriteLine($"소지한 {Item_Name[i]}의 개수가 부족합니다.");
+                            return;
+                        }
+                    }
+                }
+                Console.WriteLine($"{s}는 인벤토리에 없습니다.");
+            }
+            public void Show_Inventory()
+            {
+                bool is_Empty = true;
+                Console.WriteLine("현재 인벤토리");
+                for (int i = 0; i < MAX_ITEMS; i++)
+                {
+                    if (Item_Name[i] != null)
+                    {
+                        Console.Write($"{Item_Name[i]} x({Item_Count[i]})\n");
+                        is_Empty = false;
+                    }
+
+                }
+                if (is_Empty)
+                {
+                    Console.WriteLine("인벤토리가 비어있음.");
+                }
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Inventory inventories = new Inventory(MAX_ITEMS);
+            inventories.Add_Item("포션", 3);
+            inventories.Add_Item("포션", 3);
+            inventories.Add_Item("칼", 1);
+            inventories.Show_Inventory();
+            inventories.Delete_Item("포션", 3);
+            inventories.Show_Inventory();
+            inventories.Delete_Item("칼", 2);
+            inventories.Show_Inventory();
+            inventories.Delete_Item("칼", 1);
+            inventories.Show_Inventory();
+            inventories.Delete_Item("방패", 1);
+            inventories.Show_Inventory();
+
+
+        }
+    }
+}
