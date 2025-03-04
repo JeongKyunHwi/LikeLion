0dbc95a6d86144fa8ebb24c3f4499d0cac5c7d07
JeongKyunHwi <rbsgnl123@gmail.com>
2025-03-04 14:30:55 +0900

    study19

---
+++ study19/study19/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+            //foreach(var num in numbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //List<string> names = new List<string> { "Alice", "Bob", "Charlie" };
+
+            //names.Add("Dave");
+            //names.Remove("Bob");
+
+            //foreach(var name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //List<int> list = new List<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //foreach (var i in list)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine(list[1]);
+            //list.Insert(1, 5);
+
+            //Console.WriteLine(list[1]);
+            //Console.WriteLine(list.IndexOf(5));
+            //Console.WriteLine(list.Count);
+
+            //Stack stack = new Stack();
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+
+            //while(stack.Count > 0)
+            //{
+            //    Console.WriteLine(stack.Pop());
+            //}
+
+            //Queue queue = new Queue();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //while (queue.Count > 0)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //}
+
+            //ArrayList arrayList = new ArrayList();
+
+            //arrayList.Add(1);
+            //arrayList.Add("Hello");
+            //arrayList.Add(3.14);
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //arrayList.Remove("Hello");
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //HashTable 클래스
+            //키-값 쌍을 저장하는 컬렉션
+            //키를 이용해 값을 빠르게 검색
+
+            //Hashtable hashtable = new Hashtable();
+
+            //hashtable.Add("Alice", 25);
+            //hashtable["Bob"] = 30;
+            //hashtable["Charlie"] = 20;
+
+            //foreach(DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+            //}
+
+            //Console.WriteLine($"Alice의 나이 : {hashtable["Alice"]}");
+            //hashtable.Remove("Bob");
+            //foreach (DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+            //}
+
+            
+
+
+
+
+        }
+    }
+}
